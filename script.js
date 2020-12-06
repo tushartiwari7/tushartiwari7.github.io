b146900547b4ec44ccaaa0c707945e10638383fa
tushartiwari0172@gmail.com <tushartiwari0172@gmail.com>
2020-12-06 17:28:25 +0530

    refactor-JS-code
    moved JS code from index.html to index.js

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle ( 'sticky', window.scrollY>0 );
+});
+
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+$(document).ready(function() {
+    $('.list').click(function(){
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.itembox').show('1000');
+        }
+        else {
+            $('.itembox').not('.'+value).hide('1000');
+            $('.itembox').filter('.' +value).show('1000');
+        }
+    });
+
+    $('.list').click(function() {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+});
